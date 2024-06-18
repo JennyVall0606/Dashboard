14a12a117961d8a50e0da2e54fb563ca0ee716f3
JennyVall0606 <jaov669@gmail.com>
2024-06-17 20:23:26 -0500

    Maquetacion y creacion de vista Home DashBoard

--- src/app/domains/home/home.component.ts
+++ src/app/domains/home/home.component.ts
@@ -5,8 +5,17 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  titulo = 'WillVelsAdmin';
+imagenPrincipal = 'https://content.revistainteriores.es/medio/2023/12/12/velas-aromaticas-artesanales_12a5b249_231212162955_1280x794.jpg';
 
-}
+  imagenListaProducto =
+    'https://cdn-icons-png.flaticon.com/512/1554/1554591.png';
+  imagenListaCategorias =
+  'https://cdn-icons-png.flaticon.com/512/2422/2422208.png';
+  imagenListaOrdenes = 'https://cdn-icons-png.flaticon.com/512/9252/9252151.png';
+  imagenUsuarios = 'https://cdn-icons-png.flaticon.com/512/32/32441.png';
+
+  }
